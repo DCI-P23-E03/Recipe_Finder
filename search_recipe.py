f88168f846eb8a8aac0208d5df7f6608fe8a502e
RubinaBallach <rubina.ballach@gmail.com>
2023-06-20 13:52:13 +0200

    merged versions

--- search_recipe.py
+++ search_recipe.py
@@ -1,22 +1,21 @@
 import csv
 import random
-#from main import file_path
+from  edit_recipe import clear_screen
 
-
-
-#file_path = '/home/user/Documents/DCI_CODE/assignments2023/RecipeNLG_dataset_smaller.csv'
-def recipe_search(file_path):
-    #global file_path
-    with open(file_path) as file:
+def recipe_search():
+    with open("/home/dci-student/projects/PythonExcercises/RecipeNLG_dataset_smaller.csv") as file:
         reader = csv.reader(file) 
 
         keyselection = input(""" What are you looking for today? 
         Please put in an ingredient of your choice: \t""") # request user input
-        recipe_list_num = []
+        recipe_list_num = [] 
         recipe_list_name = []
         recipe_count = 0
+        recipe_num = int()
+    
   
         for row in reader:
+            # looking for the keyword in different varieties
             if keyselection in row [1] or keyselection in row [2] or keyselection in row [5]or keyselection.lower() in row [1] or keyselection.lower() in row [2] or keyselection.lower() in row [5] or keyselection.capitalize() in row [1] or keyselection.capitalize() in row [2] or keyselection.capitalize() in row [5]:
                 recipe_number = row [0]
                 recipe_name = row [1]
@@ -25,46 +24,40 @@ def recipe_search(file_path):
                 recipe_count = (len(recipe_list_num))
                 #print(type(recipe_count))    
                 #print(f"{recipe_number} {recipe_name}")
-               
+
+        clear_screen()
+
         if recipe_count > 0:
-            print(f"We found {recipe_count} recipe(s) that match your search.\nThey are:\n{recipe_list_name}")
+                                
+            print(f"We found {recipe_count} recipe(s) that match your search.\nThey are:\n")
+            
+            opt = 0 # pr
+            for recipe in recipe_list_name:
+                    print('[',opt,']',recipe)
+                    opt += 1 #creates a list of dynamic values for the user to pick from
+
+            # print(recipe_list_num) Check list
+
+            flag = False
+            while flag == False:
+                choice = input("Please choose a recipe:\t")
+                try: 
+                    recipe_num = recipe_list_num[int(choice)] # Ensure user's choice is valid
+                    flag = True     
+                except IndexError:
+                    print("Please select a recipe from the list.")
+                      
+
         else:
-            print("no matches")
-
-
-            #elif keyselection not in row [1] and keyselection not in row [2] and keyselection not in row [5]:
-                #print("No recipes matching your search.")
-                #break
+            print("No recipes found matching your search.")
+       
+    return recipe_num
+       
                         
                     
 
-#search()  
+recipe_search()  
 
      
-#def search():# define search function to find recipes
-    
-    #preference = input("""Welcome to the search.
-    #Firstly do you have any dietary preferences or restrictions?
-    #[1] vegetarian
-    #[2] None"""\n)
 
-    
-    
-    #if preference == 1:
-        #if keyselection == a:
-            #random.choice(file) # give out title of random recipe
-            #print(title)
-    #elif preference == 2:
-        #print("2")
-        #if keyselection == a:
-            #random.choice(file) # give out title of random recipe
-            #print(title)
-        #elif keyselection in file("title") or keyselection in file ("ingedients") or keyselection in file ("NER")
-        #print () 
-                  
-    #else:
-        #print("Please define your preference.") 
-    
-    
-#search()
   
