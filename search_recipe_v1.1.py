34ce5dc54c50d29259e3de3a117dfc05d53f2072
RubinaBallach <rubina.ballach@gmail.com>
2023-06-20 13:01:40 +0200

    cleaned

--- search_recipe_v1.1.py
+++ search_recipe_v1.1.py
@@ -8,11 +8,12 @@ def recipe_search():
 
         keyselection = input(""" What are you looking for today? 
         Please put in an ingredient of your choice: \t""") # request user input
-        recipe_list_num = []
+        recipe_list_num = [] 
         recipe_list_name = []
         recipe_count = 0
   
         for row in reader:
+            # looking for the keyword in different varieties
             if keyselection in row [1] or keyselection in row [2] or keyselection in row [5]or keyselection.lower() in row [1] or keyselection.lower() in row [2] or keyselection.lower() in row [5] or keyselection.capitalize() in row [1] or keyselection.capitalize() in row [2] or keyselection.capitalize() in row [5]:
                 recipe_number = row [0]
                 recipe_name = row [1]
@@ -28,47 +29,25 @@ def recipe_search():
                                 
             print(f"We found {recipe_count} recipe(s) that match your search.\nThey are:\n")
             
-            opt = 1 # pr
+            opt = 0 # pr
             for recipe in recipe_list_name:
                     print('[',opt,']',recipe)
-                    opt = int(opt+1) #creates a list of dynamic values for the user to pick from
+                    opt += 1 #creates a list of dynamic values for the user to pick from
 
-            print ("Please choose a recipe you would like to display.")
+            # print(recipe_list_num) Check list
+
+            choice = input("Please choose a recipe:\t")
+            recipe_num = recipe_list_num[int(choice)]
         else:
             print("No recipes found matching your search.")
 
-
+    return recipe_num
        
                         
                     
 
-#search()  
+recipe_search()  
 
      
-#def search():# define search function to find recipes
-    
-    #preference = input("""Welcome to the search.
-    #Firstly do you have any dietary preferences or restrictions?
-    #[1] vegetarian
-    #[2] None"""\n)
 
-    
-    
-    #if preference == 1:
-        #if keyselection == a:
-            #random.choice(file) # give out title of random recipe
-            #print(title)
-    #elif preference == 2:
-        #print("2")
-        #if keyselection == a:
-            #random.choice(file) # give out title of random recipe
-            #print(title)
-        #elif keyselection in file("title") or keyselection in file ("ingedients") or keyselection in file ("NER")
-        #print () 
-                  
-    #else:
-        #print("Please define your preference.") 
-    
-    
-#search()
   
