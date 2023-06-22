d57408f28e0ec3efbe7049ec2fcc91123a8fa85d
RubinaBallach <rubina.ballach@gmail.com>
2023-06-22 11:17:14 +0200

    updated search and search to edit to work with row index instead of recipe num from the first file column

--- search_recipe.py
+++ search_recipe.py
@@ -10,7 +10,7 @@ def recipe_search():
         recipe_list_num = [] 
         recipe_list_name = []
         recipe_count = 0
-        recipe_num = int()
+
     
   
         for row in reader:
@@ -19,8 +19,8 @@ def recipe_search():
                 recipe_number = row [0]
                 recipe_name = row [1]
                 recipe_list_num.append(recipe_number) # Collect recipe numbers in a list to count
-                recipe_list_name.append(recipe_name) 
-                recipe_count = (len(recipe_list_num))
+                recipe_list_name.append(recipe_name) #Collect recipe names in a list
+                recipe_count = (len(recipe_list_num)) # Calculate how many recipes have been found
                 #print(type(recipe_count))    
                 #print(f"{recipe_number} {recipe_name}")
 
@@ -36,12 +36,14 @@ def recipe_search():
                     opt += 1 #creates a list of dynamic values for the user to pick from
 
             # print(recipe_list_num) Check list
+      
 
             flag = False
             while flag == False:
                 choice = input("Please choose a recipe:\t")
                 try: 
                     recipe_num = recipe_list_num[int(choice)] # Ensure user's choice is valid
+                   
                     flag = True     
                 except IndexError:
                     print("Please select a recipe from the list.")
@@ -49,8 +51,15 @@ def recipe_search():
 
         else:
             print("No recipes found matching your search.")
-       
-    return int(recipe_num)
+
+        with open("RecipeNLG_dataset_smaller.csv") as file:
+            reader = csv.reader(file)
+            for row in reader: # try to read out line number
+                if row[0] == recipe_num:
+                    recipe_index = reader.line_num
+                    #print(recipe_index) 
+
+    return recipe_index # give out recipe index to pass to delete function
        
                                  
 #recipe_search()   #this is for test  
