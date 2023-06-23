e3f7d44f1e0f57332a3de2fca41e2e2af0f479f6
crstns <schneeweiss.carsten@gmailcom>
2023-06-23 10:35:38 +0200

    final version

--- cookbook.py
+++ cookbook.py
@@ -8,7 +8,7 @@ file = csv.reader(open(file_path, 'r'))
 #create a dictionary
 recipe_dict = {}
 
- #loop through the csv file and add the data to the dictionary {title: {ingredients : ingredients, directions : directions, link : link, keywords : NER}}
+#loop through the csv file and add the data to the dictionary {title: {ingredients : ingredients, directions : directions, link : link, keywords : NER}}
 for row in file:
     recipe_dict[row[1]] = {"ingredients": row[2], "link": row[4], "keywords": row[6]}
 
@@ -27,37 +27,63 @@ def search_recipe():
                             \n2. Search by title
     """)
 
-    #ask user for input for recipe keyword
-    recipe_keyword = input("What recipe are you looking for? ").upper()
+    #if user chooses option 1, run search_by_ingredient function
+    if search_menu == "1":
 
-    #search for a recipe by ingredient
-    for key in recipe_dict:
-        if recipe_keyword in recipe_dict[key]["ingredients"].upper():
-            print(key)
-            print("\n")                
-            print("INGREDIENTS: ", recipe_dict[key]["ingredients"][1:-1].replace('"',''))
-            print("\n")
-            print("LINK: ", recipe_dict[key]["link"])
-            print("\n")
-            print("KEYWORDS: ", recipe_dict[key]["keywords"][1:-1].replace('"',''))
-            print("\n")
-            print("##############################################################################################################")
-            print("\n")
+        #ask user for input for recipe ingredient
+        recipe_keyword = input("What ingredients are you looking for? ").upper()
 
-    #search for a recipe by title
-    for key in recipe_dict:
-        if recipe_keyword in key.upper():
-            print(key)
-            print("\n")                
-            print("INGREDIENTS: ", recipe_dict[key]["ingredients"][1:-1].replace('"',''))
+        #search for a recipe by ingredient
+        x = 0
+        for key in recipe_dict:
+            if recipe_keyword in recipe_dict[key]["ingredients"].upper():
+                x += 1
+                print("##############################################################################################################")
+                print("\n")
+                print(key)
+                print("\n")                
+                print("INGREDIENTS: ", recipe_dict[key]["ingredients"][1:-1].replace('"',''))
+                print("\n")
+                print("LINK: ", recipe_dict[key]["link"])
+                print("\n")
+                print("KEYWORDS: ", recipe_dict[key]["keywords"][1:-1].replace('"',''))
+                print("\n")
+                print("##############################################################################################################")
+                print("\n")
+                return x
+        if x == 0:
             print("\n")
-            print("LINK: ", recipe_dict[key]["link"])
+            print(f"No match with {recipe_keyword} found :(")
             print("\n")
-            print("KEYWORDS: ", recipe_dict[key]["keywords"][1:-1].replace('"',''))
+                
+
+    #if user chooses option 2, run search_by_title function
+    if search_menu == "2":
+        
+        #ask user for input for recipe title
+        recipe_keyword = input("What title are you looking for? ").upper()
+
+        #search for a recipe by title
+        x = 0
+        for key in recipe_dict:
+            if recipe_keyword in key.upper():
+                x += 1
+                print("##############################################################################################################")
+                print("\n")
+                print(key)
+                print("\n")                
+                print("INGREDIENTS: ", recipe_dict[key]["ingredients"][1:-1].replace('"',''))
+                print("\n")
+                print("LINK: ", recipe_dict[key]["link"])
+                print("\n")
+                print("KEYWORDS: ", recipe_dict[key]["keywords"][1:-1].replace('"',''))
+                print("\n")
+                return x
+        if x == 0:
             print("\n")
-            print("##############################################################################################################")
+            print(f"No match with {recipe_keyword} found :(")
             print("\n")
-   
+                
 
 ##################################################################################################################################################################################################
 
@@ -66,24 +92,30 @@ def search_recipe():
 def add_recipe():
     #ask user for input for recipe title
     recipe_title = input("What is the title of your recipe? ")
-
     #ask user for input for recipe ingredients
     recipe_ingredients = input("What are the ingredients of your recipe? ")
-
-    #ask user for input for recipe directions
-    recipe_directions = input("What are the directions of your recipe? ")
-
     #ask user for input for recipe link
     recipe_link = input("What is the link of your recipe? ")
-
     #ask user for input for recipe keywords
     recipe_keywords = input("What are the keywords of your recipe? ")
-
     #add recipe to dictionary
-    recipe_dict[recipe_title] = {"ingredients": recipe_ingredients, "directions": recipe_directions, "link": recipe_link, "keywords": recipe_keywords}
-
+    recipe_dict[recipe_title] = {"ingredients": f"{recipe_ingredients}", "link": recipe_link, "keywords": [f"{recipe_keywords}"]}  
     #print recipe dictionary
-    print(recipe_title + recipe_dict[recipe_title])
+    print("\n")
+    print("##############################################################################################################")
+    print("\n")
+    print(recipe_title)
+    print("\n")                
+    print("INGREDIENTS: ", recipe_ingredients)
+    print("\n")
+    print("LINK: ", recipe_link)
+    print("\n")
+    print("KEYWORDS: ", recipe_keywords)
+    print("\n")
+    print("##############################################################################################################")
+    print("\n")
+
+
 
 ###################################################################################################################################################################################################
 
@@ -91,24 +123,28 @@ def add_recipe():
 def edit_recipe():
     #ask user for input for recipe title
     recipe_title = input("What is the title of your recipe? ")
-
     #ask user for input for recipe ingredients
     recipe_ingredients = input("What are the ingredients of your recipe? ")
-
-    #ask user for input for recipe directions
-    recipe_directions = input("What are the directions of your recipe? ")
-
     #ask user for input for recipe link
     recipe_link = input("What is the link of your recipe? ")
-
     #ask user for input for recipe keywords
     recipe_keywords = input("What is the keyword of your recipe? ")
-
     #add recipe to dictionary
-    recipe_dict[recipe_title] = {"ingredients": recipe_ingredients, "directions": recipe_directions, "link": recipe_link, "keywords": recipe_keywords}
-
+    recipe_dict[recipe_title] = {"ingredients": f"{recipe_ingredients}", "link": recipe_link, "keywords": [f"{recipe_keywords}"]}  
     #print recipe dictionary
-    print(recipe_dict[recipe_title])
+    print("\n")
+    print("##############################################################################################################")
+    print("\n")
+    print(recipe_title)
+    print("\n")                
+    print("INGREDIENTS: ", recipe_ingredients)
+    print("\n")
+    print("LINK: ", recipe_link)
+    print("\n")
+    print("KEYWORDS: ", recipe_keywords)
+    print("\n")
+    print("##############################################################################################################")
+    print("\n")
 
 
 ###################################################################################################################################################################################################
@@ -117,12 +153,16 @@ def edit_recipe():
 def delete_recipe():
     #ask user for input for recipe title
     recipe_title = input("What is the title of your recipe? ")
-
     #delete recipe from dictionary
     recipe_dict.pop(recipe_title, None)
-
     #print recipe dictionary
-    print(recipe_dict)
+    print("\n")
+    print("##############################################################################################################")
+    print("\n")
+    print(f"{recipe_title} deleted :)")
+    print("\n")
+    print("##############################################################################################################")
+    print("\n")
 
 ###################################################################################################################################################################################################
 
@@ -137,28 +177,22 @@ while True:
                     \n4. Delete a recipe
                     \n5. Exit
     """)
-
     #if user chooses option 1, run search_recipe function
     if menu == "1":
         search_recipe()
-
     #if user chooses option 2, run add_recipe function
     elif menu == "2":
         add_recipe()
-
     #if user chooses option 3, run edit_recipe function
     elif menu == "3":
         edit_recipe()
-
     #if user chooses option 4, run delete_recipe function
     elif menu == "4":
         delete_recipe()
-
     #if user chooses option 5, exit the program
     elif menu == "5":
         print("Goodbye!")
         break
-
     #if user chooses an option that is not on the menu, print error message
     else:
         print("That is not an option. Please choose an option from the menu.")
